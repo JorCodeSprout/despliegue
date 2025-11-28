e333d4829ec2a03cef50f38705356913c0172ca2
JPradillo <a23prhijo@iesgrancapitan.org>
2025-11-28 22:34:35 +0100

    Prueba 65 de despliegue en Render

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
 # Esperar a que el servicio 'app' (PHP-FPM) sea accesible.
-# Este bucle garantiza que el hostname 'app' sea resoluble antes de iniciar Nginx.
 echo "Esperando a que el host 'app' sea resoluble en la red interna..."
 until ping -c 1 app > /dev/null 2>&1; do
-  echo "Host 'app' no encontrado aún. Reintentando en 2 segundos..."
-  sleep 2
+  echo "Host 'app' no encontrado aún. Reintentando en 20 segundos..."
+  sleep 20
 done
 
 echo "Host 'app' encontrado. Iniciando Nginx..."
